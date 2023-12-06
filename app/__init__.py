bfc72cb86a92a9def8142fa49686be35d2221a01
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2023-12-06 08:54:27 -0800

    edited __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
 from flask import Flask
 
 app = Flask(__name__)
-openai_api_key = 'your_api_key_here'  # Replace with your OpenAI API key
+openai_api_key = $openai_api_key  # Replace with your OpenAI API key
